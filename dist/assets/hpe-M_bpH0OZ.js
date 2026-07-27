194e40bbb3b50b17605369592c931292a9f2a88d
devtakkekar <devtakkekar123@example.com>
2026-07-27 10:26:00 +0530

    Ignore node_modules and dist

--- dist/assets/hpe-M_bpH0OZ.js
+++
@@ -1,1 +0,0 @@
-import{g as t}from"./main-BDzXA9B8.js";import{r as o}from"./faq-C5asFSdi.js";document.addEventListener("DOMContentLoaded",()=>{const e=t("hpe");e&&o("faq-container",e.faq)});
--- dist/assets/rhel-eOniIiur.js
+++
@@ -1,1 +0,0 @@
-import{g as r}from"./main-BDzXA9B8.js";import{r as t}from"./faq-C5asFSdi.js";document.addEventListener("DOMContentLoaded",()=>{const e=r("rhel");e&&t("faq-container",e.faq)});
